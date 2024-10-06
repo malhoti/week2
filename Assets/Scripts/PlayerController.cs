fdbff296dfa6b67566b32a0557af341139e75746
Jenish Gurung <jn171103@gmail.com>
2024-10-07 00:13:59 +0100

    Added score/win UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,54 @@
-using System.Collections ;
-using System.Collections.Generic ;
-using UnityEngine ;
-using UnityEngine.InputSystem ;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro; // Corrected casing
 
 public class PlayerController : MonoBehaviour
 {
-
     public Vector2 moveValue;
     public float speed;
-    private int count;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
-    void OnMove(InputValue value)
-    {
-        moveValue = value.Get<Vector2> ();
-    }
-    void FixedUpdate()
-        {
-            Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-        
-            GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
-        
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "PickUp"){
-            other.gameObject.SetActive(false);
-            count++;
-        }
-    }
+    private int count;
+    private int numPickups = 7; // Total number of pickups
+
     void Start()
     {
         count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You win!";
+        }
     }
 }
-
